fc9fe96bc09c399f6430b16b3895b1fc7f83b24b
duchenyang <duchenyang@hangsheng.com.cn>
2021-01-06 10:47:03 +0800

    删除冗余module

--- AndroidApp/app/src/main/java/com/sz/huanxue/androidapp/ui/activity/DemoActivity.java
+++ AndroidApp/app/src/main/java/com/sz/huanxue/androidapp/ui/activity/DemoActivity.java
@@ -1,10 +1,9 @@
 package com.sz.huanxue.androidapp.ui.activity;
 
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Handler.Callback;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
@@ -13,69 +12,43 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
-import androidx.appcompat.app.AppCompatActivity;
+
 import com.sz.huanxue.androidapp.R;
+import com.sz.huanxue.androidapp.ui.view.ArcRadioSeekBar;
+import com.sz.huanxue.androidapp.ui.view.ScaleBarViewRH5;
+import com.sz.huanxue.androidapp.utils.LogUtils;
+
+import java.util.Random;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
- *
  * @author huanxue
  * Created by Administrator on 2019/7/16.
  */
-public class DemoActivity extends AppCompatActivity implements OnClickListener {
+public class DemoActivity extends AppCompatActivity implements OnClickListener, Handler.Callback {
 
     private static final int THREAD_DELAY = 0X000001;
-    volatile Thread1 mThread1 = new Thread1();
-    Thread2 mThread2 = new Thread2();
-    Handler mHandler = new Handler(new Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
-            switch (msg.what) {
-                case THREAD_DELAY:
-                    mThread1.run();
-                    mThread2.run();
-                    mHandler.sendEmptyMessageDelayed(THREAD_DELAY, 5000);
-                    break;
-            }
-            return false;
-        }
-    });
+    private static final String TAG = DemoActivity.class.getSimpleName();
+    private final Handler mHandler = new Handler(this);
     private Button mButton;
+    private Button mButton2;
+    private Button mButton3;
+    private Button mButtonShow;
     private ProgressBar mPbSearching;
     private RadioButton mRbButton;
     private CheckBox mCbButton;
-
+    private ArcRadioSeekBar mArcRadioSeekBar;
+    private int pro = 0;
+    private ScaleBarViewRH5 mScaleBarViewRH5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("logcat", "DemoActivity------onCreate");
         setContentView(R.layout.activity_demo);
-//        mHandler.sendEmptyMessage(THREAD_DELAY);
-
-
         initView();
 
-        findViewById(R.id.button).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DemoActivity.this, SecondActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-
-            }
-        });
-        findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        findViewById(R.id.button3).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 
     @Override
@@ -110,48 +83,60 @@ public class DemoActivity extends AppCompatActivity implements OnClickListener {
 
     private void initView() {
         mButton = (Button) findViewById(R.id.button);
+        mButton2 = (Button) findViewById(R.id.button2);
+        mButton3 = (Button) findViewById(R.id.button3);
         mPbSearching = (ProgressBar) findViewById(R.id.pb_searching);
 //        mRbButton = (RadioButton) findViewById(R.id.rb_button);
 //        mCbButton = (CheckBox) findViewById(R.id.cb_button);
-
+        mButtonShow = findViewById(R.id.button_show);
+        mArcRadioSeekBar = findViewById(R.id.seekbar_arc);
+        mScaleBarViewRH5 = findViewById(R.id.scaleView);
         mButton.setOnClickListener(this);
+        mButton2.setOnClickListener(this);
+        mButton3.setOnClickListener(this);
+        mButtonShow.setOnClickListener(this);
 
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.button_show:
+                pro = 0;
+                mHandler.sendEmptyMessage(1);
+                break;
             case R.id.button:
-
+                break;
+            case R.id.button2:
+                int fm = new Random().nextInt() * (10800 - 8750 + 1) + 8750;
+                mScaleBarViewRH5.initViewParam(fm, ScaleBarViewRH5.MOD_TYPE_FM);
+                LogUtils.d(TAG + " initViewParam  MOD_TYPE_FM:" + fm);
+                break;
+            case R.id.button3:
+                int am = new Random().nextInt() * (1602 - 531 + 1) + 531;
+                mScaleBarViewRH5.initViewParam(am, ScaleBarViewRH5.MOD_TYPE_AM);
+                LogUtils.d(TAG + "  initViewParam  MOD_TYPE_AM:" + am);
                 break;
         }
     }
 
-    public class Thread1 extends Thread {
-
-        private int mCount = 0;
-
-        public int getCount() {
-            return mCount;
+    @Override
+    public boolean handleMessage(Message msg) {
+        switch (msg.what) {
+            case 1:
+                LogUtils.d(TAG + "  handleMessage  setProgress:" + pro);
+                mArcRadioSeekBar.setProgress(pro);
+                pro = pro + 10;
+                if (pro > mArcRadioSeekBar.getMax()) {
+                    return false;
+                }
+                mHandler.sendEmptyMessageDelayed(1, 500);
+                break;
+            case 2:
+                break;
         }
 
-        @Override
-        public void run() {
-
-            mCount++;
-            System.out.println();
-            Log.i("logcat", "Thread1 :" + mCount);
-            if (mCount >= 10) {
-                return;
-            }
-        }
-    }
-
-    public class Thread2 extends Thread {
-
-        @Override
-        public void run() {
-            Log.i("logcat", "Thread2 :" + mThread1.getCount());
-        }
+        return false;
     }
 }
